038f192caa78f1afdea2a1d38d54590b512fc9db
Adrian Kubała <adrian.kubala@gmail.com>
2017-03-23 15:36:02 +0100

    Ukończenie widoku listy konwersacji (#29)
    * Update gitignore file
    
    * Fix IDE issues
    
    * Remove duplicated gitignore
    
    * Update gitignore file
    
    * Add icons' references
    
    * Delete old layout for signup activity
    
    * Create LoginActivity class and add it to the Manifest
    
    * Create SignupActivity class and add it to the Manifest
    
    * Create activity from template
    
    * Adjust floating action button
    
    * Add search view to the toolbar
    
    * Update string resources
    
    * Set SearchView hint text
    
    * Add recycler view to the layout
    
    * Create recycler view item layout and contact’s name
    
    * Add lastMessageTextView
    
    * Set list item
    
    * Add gray divider to the bottom of the item
    
    * Add new view called Divider to styles.xml
    
    * Add IDs to the recycler view and divider

---
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/LoginActivity.java
@@ -0,0 +1,19 @@
+package pl.komunikator.komunikator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by adrian on 23.03.2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
