e9831ca8aab6f0b377ff823f3eae71e82e748209
Adrian Kubała <adrian.kubala@gmail.com>
2017-03-30 18:20:08 +0200

    Ukończenie wyglądu konwersacji i rozmów (#6)

---
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/ChatMessage.java
@@ -0,0 +1,13 @@
+package pl.komunikator.komunikator;
+
+/**
+ * Created by adrian on 30.03.2017.
+ */
+
+public class ChatMessage {
+    public long id;
+    public boolean isMe;
+    public String message;
+    public Long userId;
+    public String dateTime;
+}
