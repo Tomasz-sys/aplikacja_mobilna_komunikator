5be598ff86c8c858bb7fe31b397357e312134f71
Przemysław Potaczek <potaczek2626@gmail.com>
2017-04-12 21:14:10 +0200

    reverse change

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/ListActivity.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/ListActivity.java
@@ -3,8 +3,6 @@ package pl.komunikator.komunikator;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,8 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 public class ListActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -87,30 +83,20 @@ public class ListActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment = null;
-        Class fragmentClass = null;
 
         if (id == R.id.nav_contacts) {
-            fragmentClass=SettingsActivity.class;
-
+            // Handle the camera action
         } else if (id == R.id.nav_conversations) {
-            fragmentClass = SettingsActivity.class;
-        } else if (id == R.id.nav_search) {
-            fragmentClass = SettingsActivity.class;
-        } else if (id == R.id.nav_settings) {
-                fragmentClass = SettingsActivity.class;
-        } else if (id == R.id.nav_help) {
-            fragmentClass = SettingsActivity.class;
-        }
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
 
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_help) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
--- Komunikator/app/src/main/java/pl/komunikator/komunikator/SettingsActivity.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/SettingsActivity.java
@@ -1,16 +1,13 @@
 package pl.komunikator.komunikator;
 
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class SettingsActivity extends Fragment {
+public class SettingsActivity extends AppCompatActivity {
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_settings, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
     }
 }
