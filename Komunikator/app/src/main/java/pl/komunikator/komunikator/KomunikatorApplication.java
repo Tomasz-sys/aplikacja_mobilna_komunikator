d4600dcc603d36288610a24ddff33ae80c378cfc
Teneusz <teneusz@wp.pl>
2017-04-17 13:39:07 +0200

    Move realm configuration to KomunikatorAplication class

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/KomunikatorApplication.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/KomunikatorApplication.java
@@ -1,6 +1,7 @@
 package pl.komunikator.komunikator;
 
 import android.app.Application;
+import android.app.ProgressDialog;
 import android.util.Log;
 
 import io.realm.ObjectServerError;
@@ -15,10 +16,57 @@ import io.realm.SyncUser;
 public class KomunikatorApplication extends Application {
     public static String REALM_URL = "realm://81.2.250.203:9080/~/komunikator";
     public static String authURL = "http://81.2.250.203:9080/auth";
+    private SyncUser user;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
+        Realm.init(this);
+        user = SyncUser.currentUser();
+        if (user == null) {
+            final ProgressDialog progressDialog = new ProgressDialog(KomunikatorApplication.this);
+            progressDialog.setIndeterminate(true);
+            progressDialog.setMessage("Authenticating...");
+            progressDialog.show();
+            SyncCredentials syncCredentials = SyncCredentials.usernamePassword("user", "user", false);
+            Log.e("LoginActivity", syncCredentials.getUserIdentifier());
+
+            SyncUser.Callback callback = new SyncUser.Callback() {
+                @Override
+                public void onSuccess(SyncUser user) {
+                    progressDialog.dismiss();
+                    onLoginSuccess(user);
+                }
+
+                @Override
+                public void onError(ObjectServerError error) {
+                    progressDialog.dismiss();
+                    String errorMsg;
+                    switch (error.getErrorCode()) {
+                        case UNKNOWN_ACCOUNT:
+                            errorMsg = "Account does not exists.";
+                            break;
+                        case INVALID_CREDENTIALS:
+                            errorMsg = "User name and password does not match";
+                            break;
+                        default:
+                            errorMsg = error.toString();
+                    }
+                    onLoginFailed(errorMsg);
+                }
+            };
+
+            SyncUser.loginAsync(syncCredentials, KomunikatorApplication.authURL, callback);
+
+        }
+    }
+
+    private void onLoginSuccess(SyncUser user) {
+        this.user = user;
+    }
+
+    private void onLoginFailed(String errorMsg) {
+        Log.e("KomunikatorApplication", errorMsg);
     }
 }
--- Komunikator/app/src/main/java/pl/komunikator/komunikator/LoginActivity.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/LoginActivity.java
@@ -1,6 +1,5 @@
 package pl.komunikator.komunikator;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +10,8 @@ import android.widget.EditText;
 import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 
-import io.realm.ObjectServerError;
 import io.realm.Realm;
 import io.realm.SyncConfiguration;
-import io.realm.SyncCredentials;
 import io.realm.SyncUser;
 import pl.komunikator.komunikator.entity.User;
 
@@ -32,46 +29,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        Realm.init(this);
         user = SyncUser.currentUser();
-        if (user == null) {
-            final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
-            progressDialog.setIndeterminate(true);
-            progressDialog.setMessage("Authenticating...");
-            progressDialog.show();
-            SyncCredentials syncCredentials = SyncCredentials.usernamePassword("user","user",false);//.google("530614858352-f0g0cqkkpoui9926nki1dsjl8b300t90.apps.googleusercontent.com");
-            Log.e("LoginActivity", syncCredentials.getUserIdentifier());
+        SyncConfiguration config = new SyncConfiguration.Builder(user, KomunikatorApplication.REALM_URL).build();
 
-            SyncUser.Callback callback = new SyncUser.Callback() {
-                @Override
-                public void onSuccess(SyncUser user) {
-                    progressDialog.dismiss();
-                    onLoginSuccess(user);
-                }
-
-                @Override
-                public void onError(ObjectServerError error) {
-                    progressDialog.dismiss();
-                    String errorMsg;
-                    switch (error.getErrorCode()) {
-                        case UNKNOWN_ACCOUNT:
-                            errorMsg = "Account does not exists.";
-                            break;
-                        case INVALID_CREDENTIALS:
-                            errorMsg = "User name and password does not match";
-                            break;
-                        default:
-                            errorMsg = error.toString();
-                    }
-                    onLoginFailed(errorMsg);
-                }
-            };
-
-            SyncUser.loginAsync(syncCredentials, KomunikatorApplication.authURL, callback);
-
-
-        }
+        realm = Realm.getInstance(config);
 
 
         login = (EditText) findViewById(R.id.loginEditText);
@@ -79,13 +40,6 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    private void onLoginSuccess(SyncUser user) {
-        this.user = user;
-    }
-
-    private void onLoginFailed(String errorMsg) {
-        Log.e("LoginActivity", errorMsg);
-    }
 
     public void signIn(View view) {
         SyncConfiguration config = new SyncConfiguration.Builder(user, KomunikatorApplication.REALM_URL)
@@ -109,7 +63,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void signUp(View view) {
-        Intent intent = new Intent(getApplicationContext(),SignupActivity.class);
+        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
         startActivity(intent);
     }
 }
