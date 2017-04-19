b24755fc4a192ecd1f99a46ab8ce1d77ffae6500
Przemysław Potaczek <Przemysaw Potaczek>
2017-04-19 09:04:50 +0200

    fix import

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/SettingsFragment.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/SettingsFragment.java
@@ -1,7 +1,7 @@
 package pl.komunikator.komunikator;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +19,6 @@ public class SettingsFragment extends Fragment {
     public static SettingsFragment newInstance() {
 
         Bundle args = new Bundle();
-
         SettingsFragment fragment = new SettingsFragment();
         fragment.setArguments(args);
         return fragment;
