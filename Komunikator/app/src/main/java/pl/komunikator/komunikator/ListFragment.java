2283976baac56fea193f2b9100247ed89447a0d4
Teneusz <teneusz@wp.pl>
2017-04-20 09:44:08 +0200

    Remove using FloatButton in ListFragment.java

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/ListFragment.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/ListFragment.java
@@ -13,9 +13,7 @@ import android.view.ViewGroup;
 public class ListFragment extends Fragment {
 
     public static ListFragment newInstance() {
-        Bundle args = new Bundle();
         ListFragment fragment = new ListFragment();
-        fragment.setArguments(args);
         return fragment;
     }
 
@@ -28,14 +26,14 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.list_fragment, container, false);
 
-        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         return view;
     }
 }
