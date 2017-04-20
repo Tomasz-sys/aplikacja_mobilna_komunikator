66a1ad9d7ea32c069d1a12e662557607ca92a98f
Teneusz <teneusz@wp.pl>
2017-04-20 09:05:15 +0200

    Remove KomunikatorApplication.java Add StartActivity.java add check permission

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/LoginActivity.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/LoginActivity.java
@@ -3,7 +3,6 @@ package pl.komunikator.komunikator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,8 +10,6 @@ import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
 
 import io.realm.Realm;
-import io.realm.SyncConfiguration;
-import io.realm.SyncUser;
 import pl.komunikator.komunikator.entity.User;
 
 /**
@@ -22,31 +19,19 @@ import pl.komunikator.komunikator.entity.User;
 public class LoginActivity extends AppCompatActivity {
 
     private EditText login, password;
-    private Realm realm;
-    private SyncUser user;
+    private Realm realm = Realm.getDefaultInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        user = SyncUser.currentUser();
-        SyncConfiguration config = new SyncConfiguration.Builder(user, KomunikatorApplication.REALM_URL).build();
-
-        realm = Realm.getInstance(config);
-
 
         login = (EditText) findViewById(R.id.loginEditText);
         password = (EditText) findViewById(R.id.passwordEditText);
 
     }
 
-
     public void signIn(View view) {
-        SyncConfiguration config = new SyncConfiguration.Builder(user, KomunikatorApplication.REALM_URL)
-                .build();
-
-        // This will automatically sync all changes in the background for as long as the Realm is open
-        realm = Realm.getInstance(config);
         User user = realm.where(User.class)
                 .equalTo("username", login.getText().toString())
                 .equalTo("password", Hashing.sha1().hashString(password.getText().toString(), Charsets.UTF_8).toString())
