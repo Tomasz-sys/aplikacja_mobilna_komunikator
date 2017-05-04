94e8b338c6811ba3ffb573a660a4148cf20770e2
Adrian Kubała <adrian.kubala@gmail.com>
2017-05-05 01:30:30 +0200

    Add friend and prevent from adding him again

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/SearchedUsersAdapter.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/SearchedUsersAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import io.realm.Realm;
 import pl.komunikator.komunikator.entity.User;
 
 /**
@@ -48,10 +49,27 @@ public class SearchedUsersAdapter extends RecyclerView.Adapter<SearchedUsersAdap
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
-        User user = userList.get(position);
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        final User user = userList.get(position);
         holder.name.setText(user.getUsername());
         holder.email.setText(user.getEmail());
+        holder.addFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User loggedUser = User.getLoggedUser();
+                List<User> loggedUserFriends = loggedUser.friends;
+
+                Realm realm = Realm.getDefaultInstance();
+                realm.beginTransaction();
+                loggedUserFriends.add(user);
+                user.friends.add(loggedUser);
+                realm.commitTransaction();
+
+                userList.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position, getItemCount());
+            }
+        });
     }
 
     @Override
