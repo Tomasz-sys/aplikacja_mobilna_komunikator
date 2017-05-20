cd4b5c199122728fa0eb209226f2d12892f3fcae
Adrian Kubała <adrian.kubala@gmail.com>
2017-05-20 23:54:37 +0200

    Implement onCreateView

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/ConversationsFragment.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/ConversationsFragment.java
@@ -3,21 +3,27 @@ package pl.komunikator.komunikator;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ConversationsFragment extends Fragment {
+import static io.realm.internal.SyncObjectServerFacade.getApplicationContext;
 
-    public ConversationsFragment() {
-        // Required empty public constructor
-    }
+public class ConversationsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversations, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversations, container, false);
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.conversationsView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        return view;
     }
 
 }
