50ebf0e0342fc77c6e323658f198c477f31fcc5b
Adrian Kubała <adrian.kubala@gmail.com>
2017-05-24 13:33:25 +0200

    Move fragment_chat content to activity_conversation and delete unnecessary files

--- Komunikator/app/src/main/java/pl/komunikator/komunikator/activity/ConversationActivity.java
+++ Komunikator/app/src/main/java/pl/komunikator/komunikator/activity/ConversationActivity.java
@@ -25,7 +25,7 @@ public class ConversationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_chat);
+        setContentView(R.layout.activity_conversation);
 
         initControls();
     }
