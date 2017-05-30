068e739290e9a61bd6190580d86bcad49fd9780c
Przemysław Potaczek <potaczek2626@gmail.com>
2017-05-30 23:35:03 +0200

    Dokumentacja projektu

---
+++ docs/Dokumentacja Klas/html/classandroid_1_1support_1_1constraint_1_1_r_1_1id.js
@@ -0,0 +1,11 @@
+var classandroid_1_1support_1_1constraint_1_1_r_1_1id =
+[
+    [ "all", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#aea0787d90242ff600326d61d470a83f7", null ],
+    [ "basic", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#a8a7dca908a299e16c3c1e5e35fe5fcf5", null ],
+    [ "chains", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#aa18f9e706161916b0e2fe50b9af0a42f", null ],
+    [ "none", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#acb2e5557c4f8d835a63873863917b5cf", null ],
+    [ "packed", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#a6f318bd8cf4a4a7b6bb91ae39aafc8c6", null ],
+    [ "parent", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#ad1010c30c224329a8e25e8b2721b73ff", null ],
+    [ "spread", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#a19ff6e1cce6df7438948c99f48d6a96c", null ],
+    [ "spread_inside", "classandroid_1_1support_1_1constraint_1_1_r_1_1id.html#abe08ca72966af7af713a9537e4a0ff48", null ]
+];
---
+++ docs/Dokumentacja Klas/html/classandroid_1_1support_1_1design_1_1_r_1_1bool.js
@@ -0,0 +1,8 @@
+var classandroid_1_1support_1_1design_1_1_r_1_1bool =
+[
+    [ "abc_action_bar_embed_tabs", "classandroid_1_1support_1_1design_1_1_r_1_1bool.html#a12b8abdc03b45f744e5cd022a682e181", null ],
+    [ "abc_allow_stacked_button_bar", "classandroid_1_1support_1_1design_1_1_r_1_1bool.html#a2a8dac5f8c4ccac5b125012665593d60", null ],
+    [ "abc_config_actionMenuItemAllCaps", "classandroid_1_1support_1_1design_1_1_r_1_1bool.html#a4877609fc5e66b66c02146bb21a07830", null ],
+    [ "abc_config_closeDialogWhenTouchOutside", "classandroid_1_1support_1_1design_1_1_r_1_1bool.html#a8dd305af540f7f30274ca9c611c2835d", null ],
+    [ "abc_config_showMenuShortcutsWhenKeyboardPresent", "classandroid_1_1support_1_1design_1_1_r_1_1bool.html#a4042d6c58489ca07bb71f1b08b2e75bc", null ]
+];
